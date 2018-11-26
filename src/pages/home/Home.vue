8c0cd3febba5ab622b62f59268542a5e59c8d8b9
yanyanlu09 <1192360834@qq.com>
2018-11-26 17:59:41 +0800

    edit header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,21 @@
 <template>
 	<div>
 		<home-header></home-header>
+		<home-swiper></home-swiper>
+		<div>test</div>
 	</div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
-	export default{
-		name:'Home',
-		components:{
-			HomeHeader:HomeHeader
-		}
+import HomeSwiper from './components/Swiper'
+export default{
+	name:'Home',
+	components:{
+		HomeHeader:HomeHeader,
+		HomeSwiper:HomeSwiper
 	}
+}
 </script>
 
 <style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <swiper-slide>
+	    	<img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1811/9a/d7c5938126f72902.jpg_890x330_2e5663f2.jpg">
+	    </swiper-slide>
+	    <swiper-slide>
+	    	<img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1811/6f/58c1855640b17202.jpg_890x330_20a88210.jpg">
+		</swiper-slide>
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+
+<script>
+export default{
+	name:'HomeSwiper',
+	data () {
+		return {
+			swiperOption: {
+				pagination:'.swiper-pagination'
+			}
+		}
+	}
+}
+</script>
+
+<style lang='stylus' scoped>
+	.wrapper
+		overflow:hidden
+		width:100%
+		height:0
+		padding-bottom:37.08%
+		cursor:pointer
+		.swiper-image
+			width:100%
+</style>
